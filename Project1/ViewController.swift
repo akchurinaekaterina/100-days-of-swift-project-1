37370245016a4888d1095f49e5ce94c22e8fa61d
@akchurina.es <akchurina.ekaterina@gmail.com>
2020-02-18 15:06:02 +0100

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Ekaterina Musiyko on 18/02/2020.
+//  Copyright © 2020 Ekaterina Akchurina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
